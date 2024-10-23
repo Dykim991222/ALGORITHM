0d1431c87552d51ee07eb6a6909a137319d2ab23
lee-euiji <leeuj9663@naver.com>
2024-10-23 15:18:27 +0900

    241023 큰수만들기

---
+++ greedy/큰수만들기.py
@@ -0,0 +1,14 @@
+def solution(number, k):
+    result=[]
+    index = 0
+    for i in range(len(number)-k):
+        if "9" in number[index:k+i+1]: max_val = "9"
+        else: max_val = max(number[index:k+i+1])
+        for j in range(index,len(number)-1):
+            if number[j] == max_val:
+                index = j+1
+                break
+        result.append(max_val)    
+    
+    answer = "".join(result)
+    return answer
