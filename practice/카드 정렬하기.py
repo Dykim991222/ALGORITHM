98bd468d3086811bf9deff72354eef15d5161384
Dykim991222 <alfine122299@gmail.com>
2024-11-13 18:14:59 +0900

    241113 카드 정렬하기

---
+++ practice/카드 정렬하기.py
@@ -0,0 +1,27 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+N = int(input())
+numbers = []
+for _ in range(N):
+    x = int(input())
+    numbers.append(x)
+
+h = []
+for value in numbers:
+    heapq.heappush(h, value)
+
+answer = 0
+result = []
+
+
+while len(h)>1:
+    answer += heapq.heappop(h)
+    answer += heapq.heappop(h)
+    heapq.heappush(h, answer)
+    
+    result.append(answer)
+    answer = 0
+
+print(sum(result))
