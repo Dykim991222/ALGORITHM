6a4a8084783f459c3100546af90aee79b6b7a1b1
Dykim991222 <alfine122299@gmail.com>
2024-12-18 20:50:00 +0900

    2304 창고다각형

--- practice/2304_창고 다각형.py
+++ practice/2304_창고 다각형.py
@@ -1,35 +1,27 @@
 import sys
-
 input = sys.stdin.readline
 
-line_list = []
-answer = 0
+N = int(input())
+mylist = [list(map(int, input().split())) for _ in range(N)]
 
-N = int(input().strip())
-for _ in range(N):
-    L, H = map(int, input().split())
-    line_list.append([L, H])
+mylist.sort()
 
-line_list.sort()
+max_x = max(l[0] for l in mylist)
+
+height = [0] * (max_x + 1)
+
+for x, h in mylist:
+    height[x] = h
 
 max_height = 0
-for i in range(len(line_list)):
-    max_height = max(max_height, line_list[i][1])
+area = 0
+for i in range(max_x + 1):
+    max_height = max(max_height, height[i])
+    area += max_height
 
-answer += max_height
+max_height = 0
+for i in range(max_x, -1, -1):
+    max_height = max(max_height, height[i])
+    area -= max_height
 
-for i in range(len(line_list)):
-    if line_list[i][1] == max_height:
-        left = line_list[i][0]
-        right = line_list[i][0] + 1
-
-left_list = []
-right_list = []
-for i in range(len(line_list)):
-    if line_list[i][0] < left:
-        left_list.append(line_list[i])
-    if line_list[i][0] > right:
-        right_list.append(line_list[i])
-
-print(left_list)
-print(right_list)
+print(area)
