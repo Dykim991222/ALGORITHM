e4c348af653383df5111a3735e28a8de00e2b112
Dykim991222 <alfine122299@gmail.com>
2024-12-24 17:32:23 +0900

    241224 5

--- practice/17829_222풀링.py
+++ practice/17829_222풀링.py
@@ -3,8 +3,23 @@ input = sys.stdin.readline
 
 N = int(input().rstrip())
 
-total = []
-for y in range(N):
+board = []
+for _ in range(N):
     row = list(map(int, input().split()))
-    total.append(row)
-print(total)
+    board.append(row)
+
+new_board = []  
+
+for i in range(0, N, 2):  
+    new_row = []
+    for k in range(0, N, 2):  
+        matrix = [
+            board[i][k], board[i][k+1],
+            board[i+1][k], board[i+1][k+1]
+        ]
+        
+        matrix.sort(reverse=True)
+        new_row.append(matrix[1])
+    new_board.append(new_row)
+
+
